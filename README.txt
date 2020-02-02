ee6a5404aa4f95e5ac659752ab12823671926b13
Noah <Nore5515@gmail.com>
2020-02-02 06:52:26 -0600

    Added basic functionality, can set and roll dice.

--- README.txt
+++ README.txt
@@ -1,3 +1,7 @@
-// A simple, test project that will get me back on track to working on a Windows machine.
-// This will be an Odds Calculator, telling you what the likelyhood is of rolling a series of numbers on dice of your choosing.
+A simple, test project that will get me back on track to working on a Windows machine.
+This will be an Odds Calculator, telling you what the likelyhood is of rolling a series of numbers on dice of your choosing.
 
+COMPLETION CRITERIA
+The app can take in a list of dice of any sides, and run constant rolls on them.
+You can request a specific pattern of dice rolls, such as odds of 1-2 on a 6 sided dice three times.
+The app will run it for 100 times, then display the percentage (and fraction) of how many times it came up.
--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"os"
+	"math/rand"
+	"time"
+)
+
+func stoI (s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+    }
+    return i
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println("How many sides do you want your dice to have?")
+	var sidePrompt string
+	fmt.Scanln(&sidePrompt)
+	fmt.Println("And how many times do you want to roll it?")
+	var rollPrompt string
+	fmt.Scanln(&rollPrompt)
+	
+	sides := stoI(sidePrompt)
+	rolls := stoI(rollPrompt)
+	
+
+	fmt.Println("=================")
+	for i := 0; i < rolls; i++{
+		fmt.Println(rand.Intn(sides) + 1)
+	}
+	fmt.Println("=================")
+
+
+
 }
